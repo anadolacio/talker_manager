2dd3189707c410acf427292be77953feebc61d7a
anadolacio <anadolacio@hotmail.com>
2023-04-17 18:03:29 -0300

    requisito 1- resolvendo erros

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const route = express.Router();
 
-const talker = require('./talkerRoute');
+const talker = require('./talkerRoute.js');
 
 route.use(talker);
 
